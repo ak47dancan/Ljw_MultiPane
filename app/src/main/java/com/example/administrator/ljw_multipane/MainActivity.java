7b475f8480db129aba022b59348b29aa12874f3e
ak47dancan <522207348@qq.com>
2016-07-08 17:40:58 +0800

    Initial commit  helloword

---
+++ app/src/main/java/com/example/administrator/ljw_multipane/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.administrator.ljw_multipane;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
